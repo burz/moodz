958b91230056be25842f79a32cd391bce23d5718
Anthony Burzillo <burz@jhu.edu>
2014-07-14 20:16:11 -0400

    Update token last used time on authentication

--- AuthToken/Base.hs
+++ AuthToken/Base.hs
@@ -4,7 +4,7 @@ module AuthToken.Base
 ) where
 
 import Import
-import Data.Text.Encoding (decodeUtf8With)
+import Data.Text.Encoding (decodeUtf16LEWith)
 import Data.Time
 import System.Entropy
 
@@ -15,7 +15,7 @@ newToken :: UserId -> Handler AuthToken
 newToken userId = do
     t <- liftIO getCurrentTime
     b <- liftIO $ getEntropy tokenSize
-    let at = AuthToken userId t (decodeUtf8With badByteHandler b)
+    let at = AuthToken userId t (decodeUtf16LEWith badByteHandler b)
     _ <- runDB $ insert at
     return at
     where badByteHandler _ _ = Just '-'
@@ -33,7 +33,14 @@ getToken userId = do
 authenticateToken :: UserId -> Text -> Handler Bool
 authenticateToken userId value = do
     as <- runDB $ selectList [AuthTokenUser ==. userId, AuthTokenValue ==. value] []
-    return $ case as of
-        [] -> False
-        (Entity _ a) : _ -> if hasExpired a then False else True
+    case as of
+        [] -> return False
+        (Entity aid a) : _ -> if hasExpired a
+            then return False
+            else if value /= authTokenValue a
+                then return False
+                else do
+                    t <- liftIO $ getCurrentTime
+                    _ <- runDB $ update aid [AuthTokenLastUsed +=. t]
+                    return True
 
