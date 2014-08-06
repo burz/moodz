db59e93c3ce1d1ee54b18b5aa9833ebaabbc5e1a
Anthony Burzillo <burz@jhu.edu>
2014-08-05 23:58:23 -0400

    Bypass login

--- Handler/Auth.hs
+++ Handler/Auth.hs
@@ -1,11 +1,36 @@
 module Handler.Auth
-( authUser'
+( maybeAuth
+, requireAuth
+, authUser'
 , authUser
 ) where
 
 import Import
 
-import Yesod.Auth
+
+#ifdef DEVELOPMENT
+maybeAuth :: Handler (Maybe (Entity User))
+maybeAuth = do
+    let uid = Key $ PersistInt64 1
+    mu <- runDB $ get uid
+    return $ mu >>= \u -> Just $ Entity uid u
+
+requireAuth :: Handler (Entity User)
+requireAuth = do
+    let uid = Key $ PersistInt64 1
+    mu <- runDB $ get uid
+    case mu of
+        Nothing -> notFound
+        Just u -> return $ Entity uid u
+#else
+import qualified Yesod.Auth as YA
+
+maybeAuth :: Handler (Maybe (Entity User))
+maybeAuth = YA.maybeAuth
+
+requireAuth :: Handler (Entity User)
+requireAuth = YA.requireAuth
+#endif
 
 authUser' :: UserId -> (Entity User -> Handler a) -> Handler a
 authUser' userId handler = do
--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,18 +2,20 @@ module Handler.Home
 ( getHomeR
 ) where
 
+import Handler.Auth
 import Handler.Partials
 
 import Import
-import Yesod.Auth
 
 getHomeR :: Handler Html
 getHomeR = do
     auth <- maybeAuth
     case auth of
+#ifndef DEVELOPMENT
         Nothing -> defaultLayout [whamlet|
             <center>
                 <a href=@{AuthR LoginR}>Sign in or sign up|]
+#endif
         Just (Entity uid user) -> do
             defaultLayout $ do
                 setTitle "Moodz"
